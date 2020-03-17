79bd041f07d7672611415a933fc7b36ed49bf687
CCCMacro <macrofei@163.com>
2020-03-17 22:44:31 +0800

    springboot-logback

---
+++ springboot-logback/src/test/java/com/xlccc/springbootlogback/SpringbootLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlccc.springbootlogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
