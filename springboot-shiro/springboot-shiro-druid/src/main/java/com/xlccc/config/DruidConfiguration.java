15a0db95ac7fbd88f8af38b750bcfd3a30a4b2da
CCCMacro <macrofei@163.com>
2020-03-28 19:52:25 +0800

    优化

--- springboot-shiro/springboot-shiro-druid/src/main/java/com/xlccc/config/DruidConfiguration.java
+++ springboot-shiro/springboot-shiro-druid/src/main/java/com/xlccc/config/DruidConfiguration.java
@@ -28,11 +28,11 @@ public class DruidConfiguration {
     @Bean
     public ServletRegistrationBean statViewServlet() {
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
-        //白名单：
+        //白名单：(没有配置或者为空，则允许所有访问)
         servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
         //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的即提示:Sorry, you are not permitted to view this page.
         servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
-        //登录查看信息的账号密码.
+        //登录druid查看信息的账号密码.
         servletRegistrationBean.addInitParameter("loginUsername", "druid");
         servletRegistrationBean.addInitParameter("loginPassword", "12345678");
         //是否能够重置数据.
