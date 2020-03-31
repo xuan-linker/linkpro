3283ebb07e380026a299df5fa6bf582db0f318bd
CCCMacro <macrofei@163.com>
2020-03-31 14:39:51 +0800

    springboot-mongodb 优化

--- springboot-mongodb/src/test/java/com/xlccc/springbootmongodb/SpringbootMongodbApplicationTests.java
+++ springboot-mongodb/src/test/java/com/xlccc/springbootmongodb/SpringbootMongodbApplicationTests.java
@@ -1,5 +1,6 @@
 package com.xlccc.springbootmongodb;
 
+import com.xlccc.common.JsonResult;
 import com.xlccc.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
@@ -16,14 +18,27 @@ class SpringbootMongodbApplicationTests {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+
     @Test
-    public void test1 (){
+    public void test1() {
         List<User> userList = mongoTemplate.findAll(User.class);
-        if(userList != null && userList.size() > 0 ){
+        if (userList != null && userList.size() > 0) {
             userList.forEach(user -> {
                 System.out.println(user.toString());
             });
         }
+    }
+
+    @Test
+    public void test2() {
+        User user = new User();
+        user.setAddress("china");
+        user.setCreateTime(new Date());
+        user.setLastUpdateTime(new Date(3000));
+        user.setName("小明");
+        user.setPassword("admin");
+        User newUser = mongoTemplate.insert(user, "user");
+        System.out.println(new JsonResult(true, newUser));
     }
 
 }
