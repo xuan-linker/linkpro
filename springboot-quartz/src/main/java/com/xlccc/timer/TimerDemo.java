1bb70ad43433aeae485cf27302e26302cb7bc8ff
Linker <xuanlccc@gmail.com>
2020-04-17 09:36:20 +0800

    springboot-timer

---
+++ springboot-quartz/src/main/java/com/xlccc/timer/TimerDemo.java
@@ -0,0 +1,26 @@
+package com.xlccc.timer;
+
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * 基于Timer实现的定时调度（不推荐，用该方式不如用 ScheduledExecutorService ）
+ */
+public class TimerDemo {
+
+    public static void main(String[] args) {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("执行任务:" + LocalDateTime.now());
+            }
+        };
+
+        Timer timer = new Timer();
+        // timerTask：需要执行的任务
+        // delay：延迟时间（以毫秒为单位）
+        // period：间隔时间（以毫秒为单位）
+        timer.schedule(timerTask, 5000, 3000);
+    }
+}
---
+++ springboot-quartz/src/test/java/com/xlccc/springbootquartz/SpringbootQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlccc.springbootquartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
