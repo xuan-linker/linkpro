d0f93053d3b73a8ad3c75d5c6dc62188dd1931b8
Linker <xuanlccc@gmail.com>
2020-07-07 09:59:24 +0800

    Fix

--- springboot-shiro/springboot-shiro-simple/src/main/java/com/xlccc/SpringbootShiroSimpleApplication.java
+++ springboot-shiro/springboot-shiro-simple/src/main/java/com/xlccc/SpringbootShiroSimpleApplication.java
@@ -2,8 +2,9 @@ package com.xlccc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringbootShiroSimpleApplication {
 
     public static void main(String[] args) {
