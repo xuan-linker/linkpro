692a8d3c309c4945a04cec0cbe3e5b7bde256511
xuanlccc@gmail.com <xuanlccc@gmail.com>
2021-01-28 00:41:48 +0800

    springboot-swagger-config

---
+++ springboot-swagger-config/src/main/java/com/xlccc/SpringBootSwaggerConfigApplication.java
@@ -0,0 +1,16 @@
+package com.xlccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Linker
+ * @date 2021/1/28 0:38
+ * @description：
+ */
+@SpringBootApplication
+public class SpringBootSwaggerConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwaggerConfigApplication.class, args);
+    }
+}
