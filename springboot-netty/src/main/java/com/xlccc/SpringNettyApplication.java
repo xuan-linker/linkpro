ca63d91885d586650d1c13d8e08f0b7df29eeaa2
Linker <xuanlccc@gmail.com>
2022-01-15 14:25:17 +0800

    init

---
+++ springboot-netty/src/main/java/com/xlccc/SpringNettyApplication.java
@@ -0,0 +1,9 @@
+package com.xlccc;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringNettyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNettyApplication.class, args);
+    }
+}
